4411cc6969b797bcea2d3a0a454a27412309937f
ketan Kantale <147991909+ketankatale@users.noreply.github.com>
2025-06-17 18:51:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
-
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    return "Hello, World! Welcome to Flask App."
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
